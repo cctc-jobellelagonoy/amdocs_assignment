05c7c35a56cbab6897793f097cfc9e2ac3617499
jobellel <jobellel@host.docker.internal>
2022-09-19 15:53:39 +0800

    Sept 16 assignments

---
+++ src/com/training/assignment4/Task1.java
@@ -0,0 +1,32 @@
+package com.training.assignment4;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		String str = "";
+		String result = "";
+		
+		int counter;
+		
+		System.out.print("Enter String: ");
+		str = (new Scanner(System.in).next());
+		
+		for(int i=1; i<str.length(); i++) {
+			char current = str.charAt(i-1);
+			counter = 1;
+			
+			while(i < str.length() && str.charAt(i) == current) {
+				counter++;
+				i++;
+			}
+			
+			result += String.valueOf(current) + String.valueOf(counter);
+			
+		}
+		
+		System.out.print(result);
+	}
+	
+}
---
+++ src/com/training/assignment4/Task2.java
@@ -0,0 +1,70 @@
+package com.training.assignment4;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		int[][] array = getInput();
+		
+		System.out.println(spiralOrder(array));
+		
+	}
+	
+	public static int[][] getInput(){
+		Scanner input = new Scanner(System.in);
+		
+		int row, column;
+		
+		System.out.println("Row: ");
+		row = input.nextInt();
+		
+		System.out.println("Column: ");
+		column = input.nextInt();
+		
+		int[][] array = new int[row][column];
+		
+		for(int i = 0; i < row; i++) {
+			for(int j = 0; j < column; j++) {
+				array[i][j] = Integer.parseInt(input.next());
+			}
+		}
+		
+		return array;
+	}
+	
+	public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<Integer>();
+  
+        if (matrix.length == 0)
+            return ans;
+  
+        int m = matrix.length, n = matrix[0].length;
+        boolean[][] seen = new boolean[m][n];
+        int[] dr = { 0, 1, 0, -1 };
+        int[] dc = { 1, 0, -1, 0 };
+        int x = 0, y = 0, di = 0;
+  
+        for (int i = 0; i < m * n; i++) {
+            ans.add(matrix[x][y]);
+            seen[x][y] = true;
+            int cr = x + dr[di];
+            int cc = y + dc[di];
+  
+            if (0 <= cr && cr < m && 0 <= cc && cc < n
+                    && !seen[cr][cc]) {
+                x = cr;
+                y = cc;
+            } else {
+                di = (di + 1) % 4;
+                x += dr[di];
+                y += dc[di];
+            }
+        }
+        return ans;
+    }
+	
+}
